9e11e3c5d115935c01a72c857e061f6abd9e57ca
mattsyko16 <mattsyko@yahoo.co.uk>
2026-09-14 20:35:15 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.DACC_CONFIG = {
-  // Paste the public Discord Application ID here when ready.
-  discordClientId: "1549116174309130320",
-  // Optional: useful for documentation / slash command setup later.
-  discordGuildId: ""
-};
